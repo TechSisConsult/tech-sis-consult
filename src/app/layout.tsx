6fe5a3e70d2a48ec5506be0a2f62881820d0c575
Fatima Jimoh <fatimaoyiza18@gmail.com>
2026-06-17 17:31:03 +0100

    Integrate tawk to live chat and update home hero component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Raleway } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 const raleway = Raleway({
   variable: '--font-raleway',
@@ -33,6 +34,19 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a329f93c770bc1d46b1f7a3/default';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
